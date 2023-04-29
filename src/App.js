95e670c8385541d6bfbb99f10a4145ed531f8b31
parrasameer <sameer84436@gmail.com>
2023-04-29 19:01:54 +0530

    Update App.js with Map component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Sidebar from './SideBar/SideBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ContactMaker from './ContactMaker/ContactMaker';
+import ContactEdit from './ContactEdit';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Sidebar />}></Route>
+          <Route path='/contactMaker' element={<ContactMaker />}></Route>
+          <Route path='/contactEditor/:name' element={<ContactEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
